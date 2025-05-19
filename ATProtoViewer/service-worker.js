5a4b0021a5f9ee144e9e489d51d3743131b4d71c
corkiejp <33041035+corkiejp@users.noreply.github.com>
2025-05-19 14:41:23 +0100

    Prep Folder and files for PWA
    Prep Folder and files for PWA

---
+++ ATProtoViewer/service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'atproto-pwa-v1';
+const URLS_TO_CACHE = [
+  '/ATProtoViewer/',
+  '/ATProtoViewer/index.html',
+  '/ATProtoViewer/manifest.json',
+  '/ATProtoViewer/styles.css',
+  '/ATProtoViewer/main.js',
+  '/ATProtoViewer/assets/icon-192.png',
+  '/ATProtoViewer/assets/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
