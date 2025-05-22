b94892cc0a97a84932d5222ec7ea98bc7a9a083b
corkiejp <33041035+corkiejp@users.noreply.github.com>
2025-05-23 00:53:00 +0100

    Updates and improved scripts
    Updates and improved scripts

--- ATProtoViewer/service-worker.js
+++ ATProtoViewer/service-worker.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'atproto-pwa-v1.02';
+const CACHE_NAME = 'atproto-pwa-v1.03';
 const URLS_TO_CACHE = [
   '/ATProtoViewer/',
   '/ATProtoViewer/index.html',
+  '/ATProtoViewer/BlueskyPostViewer.html',
   '/ATProtoViewer/manifest.json',
   '/ATProtoViewer/styles.css',
   '/ATProtoViewer/main.js',
@@ -20,3 +21,19 @@ self.addEventListener('fetch', event => {
     caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
+
+self.addEventListener('activate', event => {
+  const cacheAllowList = [CACHE_NAME]; // Only keep the current cache
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheAllowList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
