8ac78031c866513c0751c2371ad842fe44ebe2ab
corkiejp <33041035+corkiejp@users.noreply.github.com>
2025-05-26 17:50:06 +0100

    another test of code

--- ATProtoViewer/service-worker.js
+++ ATProtoViewer/service-worker.js
@@ -11,7 +11,8 @@ const URLS_TO_CACHE = [
   '/ATProtoViewer/assets/icon-512.png'
 ];
 
-// Added share to feature for android PWA. 7th
+// Added share to feature for android PWA. 8th
+// Please do what I'm expecting?
 // Install: Cache new assets
 self.addEventListener('install', event => {
   event.waitUntil(
