c68324ee46a68af5879ccbccba2823f26124fae2
corkiejp <33041035+corkiejp@users.noreply.github.com>
2025-05-26 16:26:52 +0100

    More attempts

--- ATProtoViewer/service-worker.js
+++ ATProtoViewer/service-worker.js
@@ -11,7 +11,7 @@ const URLS_TO_CACHE = [
   '/ATProtoViewer/assets/icon-512.png'
 ];
 
-// Added share to feature for android PWA. 5th
+// Added share to feature for android PWA. 6th
 // Install: Cache new assets
 self.addEventListener('install', event => {
   event.waitUntil(
