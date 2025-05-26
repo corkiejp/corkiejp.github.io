fcdbf6e7d3d375c31a87478774e011c5788780b2
corkiejp <33041035+corkiejp@users.noreply.github.com>
2025-05-26 23:53:10 +0100

    justtesting

---
+++ Testsharetopwa/main.js
@@ -0,0 +1,6 @@
+document.getElementById('open-modal').onclick = function() {
+  document.getElementById('record-modal').style.display = 'block';
+};
+window.closeModal = function() {
+  document.getElementById('record-modal').style.display = 'none';
+};
--- Testsharetopwa/sw.js
+++ Testsharetopwa/sw.js
@@ -1,1 +1,12 @@
+const CACHE_NAME = 'testshareto-pwa-v=0.01';
+const URLS_TO_CACHE = [
+  '/Testsharetopwa/',
+  '/Testsharetopwa/index.html',
+  '/Testsharetopwa/manifest.json',
+  '/Testsharetopwa/styles.css',
+  '/Testsharetopwa/main.js',
+];
+
+
+
 self.addEventListener('fetch', () => {});
