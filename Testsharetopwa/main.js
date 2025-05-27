4db5713c6f4f16017d9c336752c2eb2ce40ccb88
John O Connell <33041035+corkiejp@users.noreply.github.com>
2025-05-27 01:13:05 +0100

    Delete Testsharetopwa directory

--- Testsharetopwa/main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('open-modal').onclick = function() {
-  document.getElementById('record-modal').style.display = 'block';
-};
-window.closeModal = function() {
-  document.getElementById('record-modal').style.display = 'none';
-};
