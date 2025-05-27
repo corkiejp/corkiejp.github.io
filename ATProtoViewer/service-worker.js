0bd6eeb878767c4790a38ffe9f5d8236cd70476c
corkiejp <33041035+corkiejp@users.noreply.github.com>
2025-05-27 03:48:52 +0100

    Bug fixes!

--- ATProtoViewer/service-worker.js
+++ ATProtoViewer/service-worker.js
@@ -36,19 +36,6 @@ self.addEventListener('activate', event => {
 });
 
 
-// Activate: Delete old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(cacheName => cacheName !== CACHE_NAME)
-          .map(cacheName => caches.delete(cacheName))
-      );
-    }).then(() => self.clients.claim()) // Control all clients immediately
-  );
-});
-
 // Fetch: Serve cached or network
 self.addEventListener('fetch', event => {
   event.respondWith(
