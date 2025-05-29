df8ab1d8d323d84500ad47bb12a43b3bbc07d060
corkiejp <33041035+corkiejp@users.noreply.github.com>
2025-05-29 12:13:52 +0100

    Bookmark autofill desc + Bug fix!
    Now have autofill for description pulled from the text of the record for bookmarks.
    
    Fix a bug with the modal for images, not scrolling after close.

--- ATProtoViewer/service-worker.js
+++ ATProtoViewer/service-worker.js
@@ -12,7 +12,7 @@ const URLS_TO_CACHE = [
 ];
 
 
-// Localstorage Bookmarks added! Need to add a line break for mobile!
+// Localstorage Bookmarks + autofill for desc! Fixed a bug that stopped scrolling on modal close.
 // Install: Cache new assets
 
 self.addEventListener('activate', event => {
