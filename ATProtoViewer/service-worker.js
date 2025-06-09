83e796ebb23639e2fec576272ad5a3418ffd08ea
corkiejp <33041035+corkiejp@users.noreply.github.com>
2025-06-09 16:24:57 +0100

    tweak to display feeds/list name

--- ATProtoViewer/service-worker.js
+++ ATProtoViewer/service-worker.js
@@ -28,6 +28,7 @@ const URLS_TO_CACHE = [
 // Integrated a view button on my lists display.
 // Bug fixes on feed/list viewer and addition of bskyinfo.com to links list.
 // Listed with bskyinfo, added a badge to the page. (3)
+// Lists/Feeds display title when available.
 // Install: Cache new assets ...
 
 self.addEventListener('activate', event => {
