73c1baa3003bc3603c9213d04f83c10ab0f3db44
corkiejp <33041035+corkiejp@users.noreply.github.com>
2025-06-13 22:41:05 +0100

    Removal of a line that was duplicating  the output

--- ttospwa/service-worker.js
+++ ttospwa/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ttospwa-v1.14';
+const CACHE_NAME = 'ttospwa-v1.15';
 const URLS_TO_CACHE = [
   '/ttospwa/',
   '/ttospwa/index.html',
@@ -12,6 +12,7 @@ const URLS_TO_CACHE = [
 // Install: Cache new assets
 // Edit in a convert of handle for plc directory
 // Forcing a re cache so not of only working on android displays
+// Included a line of code that duplicated the output.
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
