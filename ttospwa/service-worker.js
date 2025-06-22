2a557369cf81fc3e25f5b0c9972492540009addf
corkiejp <33041035+corkiejp@users.noreply.github.com>
2025-06-22 23:26:46 +0100

    Added Bluesky to MP4

--- ttospwa/service-worker.js
+++ ttospwa/service-worker.js
@@ -12,6 +12,7 @@ const URLS_TO_CACHE = [
 // Install: Cache new assets
 // ATproto.at has a link to PLC Directory 
 // Share to feature note + another tweak!
+// Bluesky to MP4 added
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
