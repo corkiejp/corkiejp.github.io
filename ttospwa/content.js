dbea8c8f54828801c87cfc995b16fdb58fb74a63
corkiejp <33041035+corkiejp@users.noreply.github.com>
2025-09-16 17:22:09 +0100

    Update content.js

--- ttospwa/content.js
+++ ttospwa/content.js
@@ -48,39 +48,6 @@ style.textContent = `
 
 
 
-style.textContent = `
-  .frame.css-ab90li-frameStyles-root {
-    position: fixed !important;
-    top: 0 !important; left: 0 !important;
-    width: 100vw !important; height: 100vh !important;
-    max-width: 100vw !important; max-height: 100vh !important;
-    min-width: 100vw !important; min-height: 100vh !important;
-    margin: 0 !important; padding: 0 !important;
-    z-index: 2147483647 !important;
-    border-radius: 0 !important;
-    background: #fff !important;
-    display: flex !important;
-    flex-direction: column !important;
-    box-sizing: border-box !important;
-  }
-
-  .css-105zbr5-frameStyles-headerWrap,
-  .css-1usqrh9-frameStyles-bodyWrap,
-  .frameHeader,
-  .frameBody {
-    width: 100% !important;
-    max-width: 100% !important;
-  }
-  .buttonClose, .closeButton {
-    position: absolute !important;
-    top: 8px !important;
-    right: 8px !important;
-    z-index: 2147483648 !important;
-    background: transparent;
-  }
-`;
-
-
 
 
 document.head.appendChild(style);
